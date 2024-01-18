4ba88d5f864dd519f240a6353396ed659ec617ae
pBhuse <pratikshabhuse2002@gmail.com>
2024-01-18 17:22:15 +0530

    17 Jan presenti sheet
    17 Jan presenti sheet

---
+++ 06_gradeSystem.js
@@ -0,0 +1,32 @@
+var gradeSystem=function(marks)
+{
+if(marks<0 || marks>100|| marks==undefined || isNaN(marks))//Invalid Input
+{
+    console.log(`${marks} Invalid Input...`);
+}
+else{
+    if(marks<=35)//fail
+    {
+        console.log(` ${marks} unfortunately you are failed...`);
+    }
+    else{
+        if(marks>=35 && marks<50)
+        {
+            console.log(`${marks} You are pass. Grade ==>c`);
+        }
+        else if(marks>=50 && marks<75)
+        {
+            console.log(`${marks} You are pass.Grade==>B`);
+        }
+        else{
+            console.log(`${marks} you are Pass. Grade ==>A`);
+        }
+    }
+}
+}
+gradeSystem(68);
+gradeSystem(35);
+gradeSystem(-10);
+gradeSystem(undefined);
+gradeSystem(null);
+gradeSystem(NaN);
