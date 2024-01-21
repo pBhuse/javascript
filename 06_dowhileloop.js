07c0eaf243c0ecb723e0322b16fb3408d1edc988
pBhuse <pratikshabhuse2002@gmail.com>
2024-01-21 23:15:45 +0530

    java script do-while loop
    java script do-while loop

---
+++ 06_dowhileloop.js
@@ -0,0 +1,33 @@
+console.log(`1 to 10 Numbers print:`);
+ var i=1;
+ do {
+    console.log(i);
+     i++;
+ } while (i<=10);
+console.log(` WAP to print numbers from 5 to 15 by incrementing 1`);
+var j=5;
+do {
+    
+    console.log(j);
+    j=j+1;
+} while (j<=15);
+console.log(` WAP to find first 10 even numbers`);
+var m=0;
+do {
+    console.log(m);
+    m=m+2;
+
+} while (m<20);
+console.log(` WAP to print table of 10 like 10 20 30 40 …… 100`);
+var n=10;
+do {
+console.log(n);
+n=n+10;
+    
+} while (n<=100);
+console.log(`WAP to print table of 10 in reverse order like → 100 90 80 70 …… 10`);
+var p=100;
+do{
+    console.log(p);
+    p=p-10;
+}while(p>=10);
