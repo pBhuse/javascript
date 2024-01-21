e005f8f433f5236a92b3e3535416e4a2d2be8549
pBhuse <pratikshabhuse2002@gmail.com>
2024-01-21 22:06:13 +0530

    javascript while loop
    javascript while loop

---
+++ 06_whileLoop.js
@@ -0,0 +1,52 @@
+console.log(`1 to 10 Numbers print`);
+var i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+console.log(`1. WAP to print numbers from 5 to 15 by incrementing 1`);
+var j = 5;
+while (j <= 15) {
+  console.log(j);
+  j = j + 1;
+}
+console.log(`2. WAP to print numbers from 50 to 40 by decrementing by 1`);
+var k = 50;
+while (k >= 40) {
+  console.log(k);
+  k = k - 1;
+}
+console.log(`3. WAP to find first 15 odd numbers `);
+var l = 1;
+
+while (l <= 30) {
+  console.log(l);
+  l = l + 2;
+}
+console.log(`4. WAP to find first 10 even numbers`);
+var m = 0;
+while (m < 20) {
+  console.log(m);
+  m = m + 2;
+}
+console.log(`5. WAP to print table of 5 like → 5 10 15 20 25 …… 50`);
+var m=5;
+while(m<=50)
+{
+    console.log(m);
+    m=m+5;
+}
+console.log(`6. WAP to print table of 10 like 10 20 30 40 …… 100`);
+n=10;
+while(n<=100)
+{
+    console.log(n);
+    n=n+10;
+}
+console.log(`7. WAP to print table of 10 in reverse order like → 100 90 80 70 …… 10`);
+var p=100;
+while(p>=10)
+{
+    console.log(p);
+    p=p-10;
+}
