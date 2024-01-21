6f4c032c9275a9eb3efc38ae4d39b44637bb8711
pBhuse <pratikshabhuse2002@gmail.com>
2024-01-21 23:31:11 +0530

    jump statement(break,continue)
    jump statement(break,continue)

---
+++ 06_jumpstatement.js
@@ -0,0 +1,27 @@
+console.log("  break statement =================== ");
+
+for (let index = 0; index <=5; index++) {
+
+    if (index==2) {
+
+        break;
+
+    }
+
+    console.log(index);
+
+}
+
+ console.log("  continue statement =================== ");
+
+for (let index = 0; index <=5; index++) {
+
+    if (index==2) {
+
+        continue;
+
+    }
+
+    console.log(index);
+
+}
